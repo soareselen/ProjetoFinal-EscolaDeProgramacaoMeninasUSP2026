2cca63c3cdcb9c809f0d6059f715e27676869c03
soareselen <elen.soares0110@gmail.com>
2026-01-30 19:20:16 +0000

    Primeiros textos e títulos básicos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,26 @@
 import streamlit as st
 
 st.title("Fibromialgia")
-st.header("Dor musculoesquelética", divider=True)
-st.subheader("Entenda o que é a sídrome, que foi reconhecida como deficiência em 2026.")
+st.header("Entenda o que é a sídrome, que foi reconhecida como deficiência em 2026.", divider=True)
+st.subheader("O que é Fibromialgia?")
 st.write(
-    "Entenda o que é a sídrome, que foi reconhecida como deficiência em 2026."
+    "Fibromialgia é uma síndrome crônica caracterizada por dor musculoesquelética generalizada, fadiga intensa, sono não reparador e alterações cognitivas (névoa mental). Sem cura definitiva, a condição afeta principalmente mulheres de 30 a 55 anos e é tratada com uma abordagem multidisciplinar, incluindo exercícios físicos, medicações e suporte psicológico para melhorar a qualidade de vida. ."
 )
+st.markdown("""---""")
+
+st.subheader("Sintomas e Características"
+             )
+st.write("- Dor Difusa: Dor constante por todo o corpo, sem sinais de inflamação local, com maior sensibilidade ao toque e pressão."
+         )
+st.write("- Distúrbios do Sono: Sono de má qualidade, fazendo com que o paciente acorde cansado."
+         )
+st.write("- Sintomas Cognitivos e Emocionais: Alterações de memória, falta de concentração, ansiedade e depressão."
+         )
+st.write("- Outros: Formigamentos, dores de cabeça, tonturas e síndrome do intestino irritável. ")
+
+st.markdown("""---""")
+
+# CÓDIGO HTML
+# html_code = """
+# <h1 style = 'color: pink>Esse é um texto rosa</h>
+# <p style = 'color:purple>Esse é um texto roxo</p>
