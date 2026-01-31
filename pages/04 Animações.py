28114ebe4d0fbb6c72dbcfb847734cfd46cfdc47
soareselen <elen.soares0110@gmail.com>
2026-01-31 12:34:59 +0000

    Testando código URL Lottie

--- pages/04 Animações.py
+++ pages/04 Animações.py
@@ -1,17 +1,19 @@
 import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
 
 st.title("Animações do Lottie no Streamlit")
 
-def carrefar_animacao(url: str):
+def carregar_animacao(url: str):
     requisicao = requests.get(url)
     if requisicao.status_code != 200:
         return None
     return requisicao.json()
 
 
-#url_animacao = COLOCAR A URL DA ANIMAÇÃO, o site da lottie,
-               #não quer logar
+url_animacao = "https://lottie.host/807e1553-6202-45e0-9118-095208479e00/87O00jF8H6.json"
+               
 
-#animacaoChat (variável da animação) = carregar_animacao(url_animacao)
-#st_lottie(animacaoChat, key="animacaoChat")
+animacaoEmail = carregar_animacao(url_animacao)
+
+st_lottie(animacaoEmail, key="animacaoEmail")
